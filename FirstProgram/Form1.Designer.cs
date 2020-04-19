5d2e0e8a7080783003d4ba3b69e07ad841d509b8
Salinda <salinda.jayasundara3010@gmail,com>
2020-04-19 14:56:43 +0530

    final commit

---
+++ FirstProgram/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace FirstProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picOriginal = new System.Windows.Forms.PictureBox();
+            this.picResult = new System.Windows.Forms.PictureBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnGray = new System.Windows.Forms.Button();
+            this.btnNegative = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picOriginal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picOriginal
+            // 
+            this.picOriginal.Location = new System.Drawing.Point(24, 28);
+            this.picOriginal.Name = "picOriginal";
+            this.picOriginal.Size = new System.Drawing.Size(421, 355);
+            this.picOriginal.TabIndex = 0;
+            this.picOriginal.TabStop = false;
+            // 
+            // picResult
+            // 
+            this.picResult.Location = new System.Drawing.Point(606, 28);
+            this.picResult.Name = "picResult";
+            this.picResult.Size = new System.Drawing.Size(399, 355);
+            this.picResult.TabIndex = 1;
+            this.picResult.TabStop = false;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(50, 433);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(135, 49);
+            this.btnOpen.TabIndex = 2;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnGray
+            // 
+            this.btnGray.Location = new System.Drawing.Point(281, 433);
+            this.btnGray.Name = "btnGray";
+            this.btnGray.Size = new System.Drawing.Size(135, 49);
+            this.btnGray.TabIndex = 3;
+            this.btnGray.Text = "Gray";
+            this.btnGray.UseVisualStyleBackColor = true;
+            this.btnGray.Click += new System.EventHandler(this.btnGray_Click);
+            // 
+            // btnNegative
+            // 
+            this.btnNegative.Location = new System.Drawing.Point(616, 433);
+            this.btnNegative.Name = "btnNegative";
+            this.btnNegative.Size = new System.Drawing.Size(135, 49);
+            this.btnNegative.TabIndex = 4;
+            this.btnNegative.Text = "Negative";
+            this.btnNegative.UseVisualStyleBackColor = true;
+            this.btnNegative.Click += new System.EventHandler(this.btnNegative_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(835, 433);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(135, 49);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1035, 520);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnNegative);
+            this.Controls.Add(this.btnGray);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.picResult);
+            this.Controls.Add(this.picOriginal);
+            this.Name = "Form1";
+            this.Text = "Image Processing";
+            ((System.ComponentModel.ISupportInitialize)(this.picOriginal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picResult)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picOriginal;
+        private System.Windows.Forms.PictureBox picResult;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnGray;
+        private System.Windows.Forms.Button btnNegative;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ FirstProgram/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace FirstProgram
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofile = new OpenFileDialog();
+            ofile.Filter = "Image File(*.bmt,*.jpg)|*.bmp;*.jpg";
+            if (DialogResult.OK == ofile.ShowDialog())
+            {
+                this.picOriginal.Image = new Bitmap(ofile.FileName);
+            }
+        }
+
+        private void btnGray_Click(object sender, EventArgs e)
+        {
+            Bitmap copy = new Bitmap((Bitmap)this.picOriginal.Image);
+
+            Processing.ConvertToGray(copy);
+            this.picResult.Image = copy;
+        }
+
+        private void btnNegative_Click(object sender, EventArgs e)
+        {
+            Bitmap copy = new Bitmap((Bitmap)this.picOriginal.Image);
+
+            Processing.ConvertToNegative(copy);
+            this.picResult.Image = copy;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "JPEG files(*.jpeg)|*.jpeg";// Saving files in JPEG Format
+                if(DialogResult.OK == sfd.ShowDialog()){
+
+                    this.picResult.Image.Save(sfd.FileName, ImageFormat.Jpeg);
+                }
+        }
+    }
+}
